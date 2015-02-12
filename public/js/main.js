91e1a42c9b07050ff1cd34bfc95111c8e19e55bc
hhartung <hermann.hartung@gmail.com>
2015-02-12 18:34:51 +0100

    add prepareUtterance function, clean up

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,7 @@
 
+
+
+
 //speech synthesis
 
 var text = document.getElementById("field");
@@ -6,25 +9,45 @@ var textResult = document.getElementById("result");
 var utterance = new SpeechSynthesisUtterance();
 var recognition = new webkitSpeechRecognition();
 var final_transcript = "";
+var speaking = false;
+var recognizing = false;
 
+
+
+
+
+
+
+var prepareUtterance = function(sText) {
+
+
+  utterance.lang = 'de-DE';
+  utterance.text = sText;
+
+  utterance.onstart = function () {
+    speaking = true;
+    console.log("start");
+    recognition.start();
+  };
+
+  utterance.onend = function() {
+    console.log("end");
+    speaking = false;
+
+  };
+
+  window.speechSynthesis.speak(utterance);
+
+}
 
 document.getElementById("speakButton").addEventListener("click", function (event) {
 	event.preventDefault();
-
-	console.log(text.value);
-
-  utterance.lang = 'de-DE';
-  utterance.text = text.value;
-	utterance.onstart = function () {
-		console.log("start");
-    recognition.start();
-	};
-
-	utterance.onend = function() {
-		console.log("end");
-	};
-
-	window.speechSynthesis.speak(utterance);
+  if(!speaking){
+  prepareUtterance(text.value);
+  }else {
+    console.log("already speaking!");
+  }
+  
 });
 
 
@@ -43,24 +66,24 @@ recognition.onresult = function(event) {
     if (event.results[i].isFinal) {
         final_transcript += event.results[i][0].transcript;
         console.log("RESULT:" + final_transcript);
-        text.innerHTML = final_transcript;
         textResult.innerHTML = final_transcript;
+
+        
         recognition.stop();
+
     }
   }
 }
 
 recognition.onend = function(event) {
-  utterance.text = final_transcript;
-  utterance.lang = 'de-DE';
-
-  window.speechSynthesis.speak(utterance);
+  r = textResult.innerHTML;
+  prepareUtterance(r);
+  console.log(r);
   final_transcript = "";
-}
-
-var prepareUtterance = function() {
 
 }
+
+
 
 
 
