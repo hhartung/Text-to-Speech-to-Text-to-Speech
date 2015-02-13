3a54a0ecb641fa457854af68a678aa89e4039827
hhartung <hermann.hartung@gmail.com>
2015-02-13 19:56:24 +0100

    push

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,38 @@
 
 
+$(document).ready(function() {
+
+  //speech synthesis
+
+  var text = document.getElementById("field");
+  var textResult = document.getElementById("result");
+  var utterance = new SpeechSynthesisUtterance();
+  var recognition = new webkitSpeechRecognition();
+  var final_transcript = "";
+  var speaking = false;
+  var recognizing = false;
+<<<<<<< HEAD
+=======
 
 
-//speech synthesis
-
-var text = document.getElementById("field");
-var textResult = document.getElementById("result");
-var utterance = new SpeechSynthesisUtterance();
-var recognition = new webkitSpeechRecognition();
-var final_transcript = "";
-var speaking = false;
-var recognizing = false;
 
 
+
+$(".headline").fadeIn(1000);
+>>>>>>> Develop
+
+  var prepareUtterance = function(sText) {
+    utterance.lang = 'de-DE';
+    utterance.text = sText;
+
+    utterance.onstart = function () {
+      speaking = true;
+      console.log("synth start");
+      recognition.start();
+    };
+
+<<<<<<< HEAD
+<<<<<<< HEAD
 var prepareUtterance = function(sText) {
   utterance.lang = 'de-DE';
   utterance.text = sText;
@@ -39,21 +59,63 @@ document.getElementById("speakButton").addEventListener("click", function (event
   prepareUtterance(text.value);
   }else {
     console.log("already speaking!");
+=======
+
+
+$(".headline").fadeIn(1000);
+
+  var prepareUtterance = function(sText) {
+    utterance.lang = 'de-DE';
+    utterance.text = sText;
+
+    utterance.onstart = function () {
+      speaking = true;
+      console.log("synth start");
+      recognition.start();
+    };
+
+=======
+>>>>>>> Develop
+    utterance.onend = function() {
+      console.log("synth end");
+      speaking = false;
+    };
+
+    window.speechSynthesis.speak(utterance);
+<<<<<<< HEAD
+>>>>>>> Develop
+=======
+>>>>>>> Develop
   }
-  
-});
+
+
+  $("#speakButton").on("click",function(){
+    if(!speaking){
+    prepareUtterance(text.value);
+    }else {
+      console.log("already speaking!");
+
+    
+    }
+    
+  });
+  //document.getElementById("speakButton").addEventListener("click", function (event) {
+  	//event.preventDefault();
+    
+    
+  //});
 
 
 
-//recognition
+  //recognition
 
 
-recognition.continuous = true;
-recognition.interimResults = true;
-recognition.lang = "de-DE";
+  recognition.continuous = true;
+  recognition.interimResults = true;
+  recognition.lang = "de-DE";
 
-
-
+<<<<<<< HEAD
+<<<<<<< HEAD
 recognition.onresult = function(event) { 
   for(i = event.resultIndex; i < event.results.length; ++i) {
     if (event.results[i].isFinal) {
@@ -69,11 +131,64 @@ recognition.onend = function(event) {
   r = textResult.innerHTML;
   prepareUtterance(r);
   final_transcript = "";
+=======
 
-}
+  recognition.onstart = function(event) {
+    console.log("rec start");
+  };
+
+  recognition.onresult = function(event) { 
+    for(i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+          final_transcript += event.results[i][0].transcript;
+          console.log("RESULT:" + final_transcript);
+          textResult.innerHTML = final_transcript;
+          $("#result").fadeIn(1000);
+          
+          recognition.stop();
+
+      }
+    }
+  }
+>>>>>>> Develop
+
+  recognition.onend = function(event) {
+    console.log("rec end");
+    r = textResult.innerHTML;
+    prepareUtterance(r);
+    final_transcript = "";
+
+=======
+
+  recognition.onstart = function(event) {
+    console.log("rec start");
+  };
+
+  recognition.onresult = function(event) { 
+    for(i = event.resultIndex; i < event.results.length; ++i) {
+      if (event.results[i].isFinal) {
+          final_transcript += event.results[i][0].transcript;
+          console.log("RESULT:" + final_transcript);
+          textResult.innerHTML = final_transcript;
+          $("#result").fadeIn(1000);
+          
+          recognition.stop();
+
+      }
+    }
+  }
+
+  recognition.onend = function(event) {
+    console.log("rec end");
+    r = textResult.innerHTML;
+    prepareUtterance(r);
+    final_transcript = "";
+
+>>>>>>> Develop
+  }
 
 
-
+});
 
 
 
